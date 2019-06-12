0b2f60419f4516ba91447ddee195bc7bd3a49343
Lars Wilhelmsen <lars@sral.org>
2019-06-12 09:20:17 +0200

    Changes to release management and created a separate directory for examples

--- RELEASE_CHECKLIST.md
+++ RELEASE_CHECKLIST.md
@@ -4,7 +4,6 @@
 1. Build locally```python setup.py sdist bdist_wheel```.
 2. Run ```tox``` to ensure that the package builds and can be used with the Python 3.x env.
 3. Update the ```CHANGELOG.rst```.
-3. Bump version number in ```VERSION``` and commit.
-4. Tag the local git repository: ```git tag x.y.z```.
+4. Run ```bumpversion [part]``` where part is either major, minor or patch.
 5. Push tags: ```git push origin master --tags```.
 6. Wait for Travis CI to publish package.
---
+++ examples/api.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2019 Lars Wilhelmsen <lars@sral.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import json
+import logging
+
+from flask import Flask, g
+from flask_oidc_ex import OpenIDConnect
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = Flask(__name__)
+app.config.update({
+    'SECRET_KEY': 'SomethingNotEntirelySecret',
+    'TESTING': True,
+    'DEBUG': True,
+    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
+    'OIDC_ID_TOKEN_COOKIE_SECURE': False,
+    'OIDC_REQUIRE_VERIFIED_EMAIL': False,
+    'OIDC_VALID_ISSUERS': [
+        'http://localhost:8080/auth/realms/demo'
+    ],
+    'OIDC_RESOURCE_SERVER_ONLY': True,
+    'OIDC_RESOURCE_SERVER_VALIDATION_MODE': 'offline',
+    'OIDC_USER_INFO_ENABLED': False,
+    'OIDC_SCOPES': [
+        'openid',
+        'email',
+        'profile',
+    ]
+})
+
+oidc = OpenIDConnect(app)
+
+@app.route('/echo')
+@oidc.accept_token(require_token=True, ['openid'])
+def echo_token_content():
+  return json.dumps(g.oidc_token_info)
+
+if __name__ == '__main__':
+    app.run()
+
